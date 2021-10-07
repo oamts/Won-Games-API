5e39bee45a29809cc8218f2937c7fefa574f1d1f
Mateus <mateusoliveiraalmeida@gmail.com>
2021-10-07 15:56:31 -0300

    Creating service populate and using another core service

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,4 +5,10 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (params) => {
+    const cat = await strapi.query('category').find({ name: "Action" });
+
+    console.log(cat)
+  }
+};
